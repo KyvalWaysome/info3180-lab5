cfcbff4349f622f79eef4c0e70a008c155ee56e9
Kyval Waysome <kyvaldhinowaysome@gmail.com>
2026-03-17 11:51:49 -0500

    Made more changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
+    }
+  },
+  server: {
+    proxy: {
+      '^/api.*': {
+        target: 'http://localhost:8080', // your Flask backend
+        changeOrigin: true,
+        secure: false
+      }
     }
   }
 })
